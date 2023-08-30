d39bbbe4d1eb9919b9c43dd4bd916b24db1c6923
Colin From <cdfrom481@gmail.com>
2023-08-30 09:54:38 -0600

    Adding assets

---
+++ food-order/src/assets/ProductList.jsx
@@ -0,0 +1,44 @@
+const ProductList = [
+  {
+    title: "Combos",
+    products: [
+      {
+        name: "Hamburger Combo",
+        description: "Our basic no fuss hamburger with fries and a soda.",
+        price: 3.5,
+      },
+      { name: "Bacon Burger Combo", description: "TBD", price: 4.5 },
+      { name: "Cheeseburger Combo", description: "TBD", price: 4.0 },
+      { name: "Bacon Cheeseburger Combo", description: "TBD", price: 5.0 },
+    ],
+  },
+  {
+    title: "Burgers",
+    products: [
+      { name: "Hamburger", description: "TBD", price: 2.0 },
+      { name: "Bacon Burger", description: "TBD", price: 3.0 },
+      { name: "Cheeseburger", description: "TBD", price: 2.5 },
+      { name: "Bacon Cheeseburger", description: "TBD", price: 3.5 },
+    ],
+  },
+  {
+    title: "Sides",
+    products: [
+      { name: "Fries", description: "TBD", price: 1.0 },
+      { name: "Cheese Fries", description: "TBD", price: 2.0 },
+      { name: "Poutine", description: "TBD", price: 3.0 },
+      { name: "Onion Rings", description: "TBD", price: 1.5 },
+    ],
+  },
+  {
+    title: "Drinks",
+    products: [
+      { name: "Cola", description: "TBD", price: 1.0 },
+      { name: "Diet Cola", description: "TBD", price: 1.0 },
+      { name: "Vanilla Shake", description: "TBD", price: 3.0 },
+      { name: "Chocolate Shake", description: "TBD", price: 3.0 },
+    ],
+  },
+];
+
+export default ProductList;
