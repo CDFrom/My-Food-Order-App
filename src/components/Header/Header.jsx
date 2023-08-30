d75e8c4c3e74252197fc6b446c84a697dc6998fd
Colin From <cdfrom481@gmail.com>
2023-08-30 14:13:47 -0600

    Added a cart to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
+import Card from "../UI/Card/Card";
 import Cart from "../../assets/Cart.svg";
 
 import classes from "./Header.module.css";
 
 const Header = (props) => {
+  const openCart = () => {};
+
   return (
     <>
       <div className={classes.header}>
         <span>Bill's Burgers, Fries & Shakes</span>
-        {/* <span>Basket</span> */}
-        <img src={Cart} alt='Shopping Cart' />
+        <Card className={classes.cart} onClick={openCart}>
+          <img src={Cart} alt='Shopping Cart' />
+          <span className={classes.totalItems}>0</span>
+        </Card>
       </div>
       <div className={classes["background-image"]} />
     </>
