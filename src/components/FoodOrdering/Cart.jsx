952a1853802f2eba6c1e2f24588a10d868b6c793
Colin From <cdfrom481@gmail.com>
2023-08-30 19:40:50 -0600

    Creating the cart

---
+++ src/components/FoodOrdering/Cart.jsx
@@ -0,0 +1,37 @@
+import { useContext } from "react";
+
+import Button from "../UI/Button/Button";
+import Card from "../UI/Card/Card";
+import CartContext from "../../context/cart-context";
+
+import classes from "./Cart.module.css";
+
+const Cart = () => {
+  const cartContext = useContext(CartContext);
+
+  const orderHandler = () => {
+    cartContext.completeOrder();
+  };
+
+  const orderList = cartContext.items.map((item) => {
+    return (
+      <div className={classes["order-item"]}>
+        <div>
+          <span>{item.name}</span> x{item.amount}
+        </div>
+        <div>${item.price}</div>
+      </div>
+    );
+  });
+
+  return (
+    <Card className={classes.cart}>
+      <div className={classes["cart__bottom"]}>
+        <div>Total Price: {cartContext.totalPrice.toFixed(2)}</div>
+        <Button onClick={orderHandler}>Order</Button>
+      </div>
+    </Card>
+  );
+};
+
+export default Cart;
