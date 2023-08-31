55624c3fdc1ab13698db12b1ab49fc435e78da48
Colin From <cdfrom481@gmail.com>
2023-08-31 14:01:07 -0600

    Added plus and minus to cart items and some styles

--- src/components/FoodOrdering/Cart.jsx
+++ src/components/FoodOrdering/Cart.jsx
@@ -13,22 +13,51 @@ const Cart = (props) => {
     cartContext.completeOrder();
   };
 
+  const amountIncrease = (item) => {
+    const itemToAdd = { ...item, amount: 1 };
+    cartContext.addItem(itemToAdd);
+  };
+
+  const amountDecrease = (item) => {
+    cartContext.removeItem(item);
+  };
+
   const orderList = cartContext.items.map((item) => {
     return (
       <div key={item.name} className={classes["order-item"]}>
         <div>
           {item.amount}x <span>{item.name}</span>
         </div>
-        <div>${(item.price * item.amount).toFixed(2)}</div>
+        <div className={classes["order-item__right"]}>
+          <div className={classes.price}>
+            ${(item.price * item.amount).toFixed(2)}
+          </div>
+          <Button
+            className={classes.plus}
+            onClick={() => {
+              amountDecrease(item);
+            }}
+          >
+            -
+          </Button>
+          <Button
+            className={classes.plus}
+            onClick={() => {
+              amountIncrease(item);
+            }}
+          >
+            +
+          </Button>
+        </div>
       </div>
     );
   });
 
   return (
     <Card className={classes.cart}>
-      <div className={classes.exit} onClick={props.onClick}>
+      <Button className={classes.exit} onClick={props.onClick}>
         X
-      </div>
+      </Button>
       <div className={classes["order-list"]}>{orderList}</div>
       <div className={classes["cart__bottom"]}>
         <div>Total Price: ${cartContext.totalPrice.toFixed(2)}</div>
