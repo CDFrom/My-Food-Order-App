2ac9484d6b25b474df333e560dd3f31ee3a22f07
Colin From <cdfrom481@gmail.com>
2023-09-01 15:15:10 -0600

    Forgot to include, needed for getting id's on Card

--- src/components/UI/Card/Card.jsx
+++ src/components/UI/Card/Card.jsx
@@ -3,6 +3,7 @@ import classes from "./Card.module.css";
 const Card = (props) => {
   return (
     <div
+      id={props.id}
       className={`${classes.card} ${props.className}`}
       onClick={props.onClick}
     >
