e06fcf9bc815affad4ee58cedcd1bf95db0b34c5
Colin From <cdfrom481@gmail.com>
2023-09-01 10:43:01 -0600

    Updating the product list

--- src/assets/ProductList.jsx
+++ src/assets/ProductList.jsx
@@ -4,39 +4,211 @@ const ProductList = [
     products: [
       {
         name: "Hamburger Combo",
-        description: "Our basic no fuss hamburger with fries and a soda.",
+        description: "One hamburger, a side of fries, and a fountain drink.",
         price: 3.5,
       },
-      { name: "Bacon Burger Combo", description: "TBD", price: 4.5 },
-      { name: "Cheeseburger Combo", description: "TBD", price: 4.0 },
-      { name: "Bacon Cheeseburger Combo", description: "TBD", price: 5.0 },
+      {
+        name: "Bacon Burger Combo",
+        description: "One bacon burger, a side of fries, and a fountain drink.",
+        price: 4.5,
+      },
+      {
+        name: "Cheeseburger Combo",
+        description: "One cheeseburger, a side of fries, and a fountain drink.",
+        price: 4.0,
+      },
+      {
+        name: "Bacon Cheeseburger Combo",
+        description:
+          "One bacon cheeseburger, a side of fries, and a fountain drink.",
+        price: 5.0,
+      },
+      {
+        name: "2 Piece Chicken Combo",
+        description:
+          "2 pieces of chicken (one light, one dark), a side of fries, and a fountain drink.",
+        price: 3.5,
+      },
+      {
+        name: "4 Piece Chicken Combo",
+        description:
+          "4 pieces of chicken (2 light, 2 dark), a side of fries, and a fountain drink.",
+        price: 5.0,
+      },
+      {
+        name: "3 Chicken Strips Combo",
+        description:
+          "3 chicken strips, sweet and sour sauce, a side of fries, and a fountain drink.",
+        price: 4.5,
+      },
+      {
+        name: "5 Chicken Strips Combo",
+        description:
+          "5 chicken strips, sweet and sour sauce, a side of fries, and a fountain drink.",
+        price: 5.5,
+      },
+      {
+        name: "6 Chicken Nugget Combo",
+        description:
+          "6 chicken nuggets, sweet and sour sauce, a side of fries, and a fountain drink.",
+        price: 4.5,
+      },
+      {
+        name: "10 Chicken Nugget Combo",
+        description:
+          "10 chicken nuggets, sweet and sour sauce, a side of fries, and a fountain drink.",
+        price: 5.5,
+      },
     ],
   },
   {
     title: "Burgers",
     products: [
-      { name: "Hamburger", description: "TBD", price: 2.0 },
-      { name: "Bacon Burger", description: "TBD", price: 3.0 },
-      { name: "Cheeseburger", description: "TBD", price: 2.5 },
-      { name: "Bacon Cheeseburger", description: "TBD", price: 3.5 },
+      {
+        name: "Hamburger",
+        description: "Good ol' fashion grilled burger.",
+        price: 2.0,
+      },
+      {
+        name: "Bacon Burger",
+        description: "Our delicious hamburger with 2 strips of bacon.",
+        price: 3.0,
+      },
+      {
+        name: "Cheeseburger",
+        description: "Our delicious hamburger topped with american cheddar.",
+        price: 2.5,
+      },
+      {
+        name: "Bacon Cheeseburger",
+        description:
+          "Our delicious hamburger topped with american cheddar and 2 strips of bacon.",
+        price: 3.5,
+      },
+    ],
+  },
+  {
+    title: "Chicken",
+    products: [
+      {
+        name: "2 Piece",
+        description: "2 pieces of hand-breaded chicken (1 light, 1 dark)",
+        price: 2.0,
+      },
+      {
+        name: "4 Piece",
+        description: "4 pieces of hand-breaded chicken (2 light, 2 dark).",
+        price: 3.5,
+      },
+      {
+        name: "3 Strips",
+        description: "3 chicken strips (comes w/ sweet and sour sauce).",
+        price: 3.0,
+      },
+      {
+        name: "5 Strips",
+        description: "5 chicken strips (comes w/ sweet and sour sauce).",
+        price: 4.0,
+      },
+      {
+        name: "6 Nuggets",
+        description: "6 chicken nuggets (comes w/ sweet and sour sauce).",
+        price: 3.0,
+      },
+      {
+        name: "10 Nuggets",
+        description: "10 chicken nuggets (comes w/ sweet and sour sauce).",
+        price: 4.0,
+      },
     ],
   },
   {
     title: "Sides",
     products: [
-      { name: "Fries", description: "TBD", price: 1.0 },
-      { name: "Cheese Fries", description: "TBD", price: 2.0 },
-      { name: "Poutine", description: "TBD", price: 3.0 },
-      { name: "Onion Rings", description: "TBD", price: 1.5 },
+      {
+        name: "Fries",
+        description: "Freshly made, hot, thick-cut french fries",
+        price: 1.0,
+      },
+      {
+        name: "Cheese Fries",
+        description:
+          "Our fresh out of the fryer fries covered in a cheese sauce.",
+        price: 2.0,
+      },
+      {
+        name: "Poutine",
+        description:
+          "A large portion of our fries topped with cheese curds and covered in gravy.",
+        price: 3.0,
+      },
+      {
+        name: "Onion Rings",
+        description: "Made to order, hand-breaded onion rings.",
+        price: 1.5,
+      },
     ],
   },
   {
     title: "Drinks",
     products: [
-      { name: "Cola", description: "TBD", price: 1.0 },
-      { name: "Diet Cola", description: "TBD", price: 1.0 },
-      { name: "Vanilla Shake", description: "TBD", price: 3.0 },
-      { name: "Chocolate Shake", description: "TBD", price: 3.0 },
+      { name: "Cola", description: "Cola fountain drink.", price: 1.0 },
+      {
+        name: "Diet Cola",
+        description: "Diet-Cola fountain drink.",
+        price: 1.0,
+      },
+      {
+        name: "Lemon-Lime",
+        description: "Lemon-Lime fountain drink.",
+        price: 1.0,
+      },
+      {
+        name: "Root Beer",
+        description: "Root Beer fountain drink.",
+        price: 1.0,
+      },
+      {
+        name: "Iced Tea",
+        description: "Fresh Iced Tea (No refills)",
+        price: 1.5,
+      },
+      {
+        name: "Lemonade",
+        description: "Fresh Lemonade (No refills)",
+        price: 1.5,
+      },
+      {
+        name: "Cherry Slush",
+        description: "Cherry flavoured slushy.",
+        price: 2.0,
+      },
+      {
+        name: "Watermelon Slush",
+        description: "Watermelon flavoured slushy.",
+        price: 2.0,
+      },
+      {
+        name: "Orange Slush",
+        description: "Orange flavoured slushy.",
+        price: 2.0,
+      },
+      {
+        name: "Blue Raspberry Slush",
+        description: "Blue raspberry flavoured slushy.",
+        price: 2.0,
+      },
+      { name: "Vanilla Shake", description: "Vanilla milkshake", price: 3.0 },
+      {
+        name: "Chocolate Shake",
+        description: "Chocolate milkshake",
+        price: 3.0,
+      },
+      {
+        name: "Strawberry Shake",
+        description: "Strawberry milkshake",
+        price: 3.0,
+      },
     ],
   },
 ];
